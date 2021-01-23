386a4662fcf696c2d9097d4fba4fdedb4287c8a5
Rajat singh <rajat.singh.se@gmail.com>
2021-01-24 00:19:46 +0530

    first2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Ques1Component } from './ques1/ques1.component';
+import { Ques2Component } from './ques2/ques2.component';
+import { Ques3Component } from './ques3/ques3.component';
+import { Ques4Component } from './ques4/ques4.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path:'ques1', component:Ques1Component},
+ {
+   path:'ques2', component:Ques2Component},
+   {
+     path:'ques3',component:Ques3Component},
+     {
+       path:'ques4', component:Ques4Component
+     },{
+      path: '**', component:Ques1Component
+     }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/ques3/ques3.component.ts
+++ src/app/ques3/ques3.component.ts
@@ -12,4 +12,53 @@ export class Ques3Component implements OnInit {
   ngOnInit(): void {
   }
 
+  num ="";
+  text="";
+  button:boolean=false;
+  count:number=2;
+
+
+
+  myArray = [
+    { id:1, name:''},
+
+  ];
+
+  removeItem(id:any){
+     this.myArray = this.myArray.filter(item => item.id !== id);
+     if(this.myArray.length<=1)
+     {
+       this.button=false;
+     }
+
+     this.text="";
+  }
+
+
+
+  addItem(){
+
+    let data={id:this.count++,name:""};
+    this.myArray.push(data);
+    this.button=true;
+    this.text="";
+
+  }
+
+
+
+  showItem(){
+
+
+    for (let i = 0; i <  this.myArray.length; i++) {
+
+      console.log("phoneNumber"+(i+1)+":"+this.myArray[i].name);
+
+
+    }
+
+this.text="cheak Console for Result";
+  }
 }
+
+
